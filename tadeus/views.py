9b3eccfecc6332bfa62551a570f05de24e4bba1b
Barbara Poszewiecka <you@example.com>
2021-12-05 22:09:07 +0100

    Formatting tadeus app

--- tadeus/views.py
+++ tadeus/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 
+
 def index(request):
-    #evals = Evaluation.objects.filter(owner__username = 'root') 
-    #table = EvalTable(evals)
-    #RequestConfig(request).configure(table)
+    # evals = Evaluation.objects.filter(owner__username = 'root')
+    # table = EvalTable(evals)
+    # RequestConfig(request).configure(table)
     table = None
-    return render(request, 'tadeus/index.html', {'table': table })
+    return render(request, "tadeus/index.html", {"table": table})
