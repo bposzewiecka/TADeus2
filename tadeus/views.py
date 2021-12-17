0c7ae97034a283926868f0cf867fa12dd9631e96
Barbara Poszewiecka <you@example.com>
2021-12-18 00:33:50 +0100

    main pange changed. Fix

--- tadeus/views.py
+++ tadeus/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from datasources.models import Chromosome
+
 from .forms import TranslocationForm
 
 
@@ -8,6 +10,8 @@ def index(request):
     # table = EvalTable(evals)
     # RequestConfig(request).configure(table)
 
+    chromosomes = Chromosome.objects.filter(assembly__name="hg38")
+
     trans_form = TranslocationForm()
 
-    return render(request, "tadeus/index.html", {"trans_form": trans_form})
+    return render(request, "tadeus/index.html", {"trans_form": trans_form, "chromosomes": chromosomes})
