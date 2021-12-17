a784cf2fb3847ac057ed63e36bd7ea919b567d25
Barbara Poszewiecka <you@example.com>
2021-12-17 23:47:15 +0100

    main pange changed

---
+++ tadeus/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+
+"""
+class CNVForm(forms.Form):
+    cnv_type = forms.CharField(max_length=1)
+    chrom =
+    start_coordinate = forms.IntegerField()
+    end_coordinate = forms.IntegerField()
+"""
+
+TRANSLOCATION_DIRECTIONS_HEAD = "H"
+TRANSLOCATION_DIRECTIONS_TAIL = "T"
+
+TRANSLOCATION_DIRECTIONS = (
+    (TRANSLOCATION_DIRECTIONS_HEAD, "Head"),
+    (TRANSLOCATION_DIRECTIONS_TAIL, "Tail"),
+)
+
+
+class TranslocationForm(forms.Form):
+    def get_chromosomes():
+        return ((1, "chr1"),)
+
+    chrom1 = forms.ChoiceField(choices=get_chromosomes)
+    coordinate1 = forms.IntegerField()
+    direction1 = forms.ChoiceField(choices=TRANSLOCATION_DIRECTIONS)
+    chrom2 = forms.ChoiceField(choices=get_chromosomes)
+    coordinate2 = forms.IntegerField()
+    direction2 = forms.ChoiceField(choices=TRANSLOCATION_DIRECTIONS)
--- tadeus/views.py
+++ tadeus/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 
+from .forms import TranslocationForm
+
 
 def index(request):
     # evals = Evaluation.objects.filter(owner__username = 'root')
     # table = EvalTable(evals)
     # RequestConfig(request).configure(table)
-    table = None
-    return render(request, "tadeus/index.html", {"table": table})
+
+    trans_form = TranslocationForm()
+
+    return render(request, "tadeus/index.html", {"trans_form": trans_form})
